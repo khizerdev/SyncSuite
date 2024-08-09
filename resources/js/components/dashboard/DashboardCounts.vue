77de78dc9f6318d7baaff3883ceac4caf02c3c6f
khizerdev <46632721+khizerdev@users.noreply.github.com>
2024-08-10 00:08:52 +0500

    Console added for url

--- resources/js/components/dashboard/DashboardCounts.vue
+++ resources/js/components/dashboard/DashboardCounts.vue
@@ -1,27 +1,23 @@
 <template>
-    <CountBox
-        :count="counts.products"
-        iconClass="ion ion-bag"
-        title="Products"
-    />
-    <CountBox
-        :count="counts.vendors"
-        iconClass="ion ion-person"
-        boxClass="bg-success"
-        title="Vendors"
-    />
-    <CountBox
-        :count="counts.customers"
-        iconClass="ion ion-person"
-        boxClass="bg-warning"
-        title="Customers"
-    />
-    <CountBox
-        :count="counts.employees"
-        iconClass="ion ion-person"
-        boxClass="bg-primary"
-        title="Employees"
-    />
+  <CountBox :count="counts.products" iconClass="ion ion-bag" title="Products" />
+  <CountBox
+    :count="counts.vendors"
+    iconClass="ion ion-person"
+    boxClass="bg-success"
+    title="Vendors"
+  />
+  <CountBox
+    :count="counts.customers"
+    iconClass="ion ion-person"
+    boxClass="bg-warning"
+    title="Customers"
+  />
+  <CountBox
+    :count="counts.employees"
+    iconClass="ion ion-person"
+    boxClass="bg-primary"
+    title="Employees"
+  />
 </template>
 
 <script setup>
@@ -31,20 +27,21 @@ import { baseUrl } from "../../utils/constants";
 import CountBox from "./CountBox.vue";
 
 const counts = ref({
-    products: 0,
-    vendors: 0,
-    customers: 0,
-    employees: 0,
+  products: 0,
+  vendors: 0,
+  customers: 0,
+  employees: 0,
 });
 
 onMounted(() => {
-    axios
-        .get(`${baseUrl}/api/products/count`)
-        .then((response) => {
-            counts.value = response.data;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  console.log(baseUrl);
+  axios
+    .get(`${baseUrl}/api/products/count`)
+    .then((response) => {
+      counts.value = response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 </script>
