999feca6d73057c2baaa557291b528b13275935f
khizerdev <46632721+khizerdev@users.noreply.github.com>
2024-12-26 00:02:21 +0500

    api env updated

--- resources/api.js
+++ resources/api.js
@@ -6,16 +6,18 @@ let baseUrl;
 
 if (webUrl == "http://localhost:8000/") {
   baseUrl = "http://localhost:8000/";
-} else if (webUrl == "https://ahmedfabrics.com.pk/paramount") {
-  baseUrl = "https://ahmedfabrics.com.pk/paramount";
-} else if (webUrl == "https://ahmedfabrics.com.pk/paramount2") {
-  baseUrl = "https://ahmedfabrics.com.pk/paramount2";
+} else if (webUrl == "https://ahmedfabrics.com.pk/paramount/") {
+  baseUrl = "https://ahmedfabrics.com.pk/paramount/";
+} else if (webUrl == "https://ahmedfabrics.com.pk/paramount2/") {
+  baseUrl = "https://ahmedfabrics.com.pk/paramount2/";
 } else {
   baseUrl = "http://localhost:8000/";
 }
 
+console.log(baseUrl)
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: baseUrl,
 });
 
 export default api;
