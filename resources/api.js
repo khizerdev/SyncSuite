a2fe2720eacf291a7bdab4375f82c9b2b29be9a6
khizerdev <46632721+khizerdev@users.noreply.github.com>
2024-12-26 00:18:22 +0500

    Api end point updated

--- resources/api.js
+++ resources/api.js
@@ -4,17 +4,18 @@ const webUrl = window.location.protocol + "//" + window.location.host;
 
 let baseUrl;
 
-if (webUrl == "http://localhost:8000/") {
+if (webUrl == "http://localhost:8000") {
   baseUrl = "http://localhost:8000/";
-} else if (webUrl == "https://ahmedfabrics.com.pk/paramount/") {
+} else if (webUrl == "https://ahmedfabrics.com.pk/paramount") {
   baseUrl = "https://ahmedfabrics.com.pk/paramount/";
-} else if (webUrl == "https://ahmedfabrics.com.pk/paramount2/") {
+} else if (webUrl == "https://ahmedfabrics.com.pk/paramount2") {
   baseUrl = "https://ahmedfabrics.com.pk/paramount2/";
 } else {
   baseUrl = "http://localhost:8000/";
 }
 
-console.log(baseUrl)
+console.log(baseUrl);
+console.log(webUrl);
 
 const api = axios.create({
   baseURL: baseUrl,
